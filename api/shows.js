2a1cf8092c1ee54417adf21e33779c7937f7dc85
Vystelior <235885728+Vystelior@users.noreply.github.com>
2025-11-17 21:54:38 +0000

    Update shows.js

--- api/shows.js
+++ api/shows.js
@@ -1,13 +1,11 @@
 // /api/shows.js
 export default async function handler(req, res) {
-    const API_KEY = process.env.TMDB_API_KEY; // set in Vercel environment variables
-    const shows = [25987, 52851]; // your TMDb IDs
-
-    const showPromises = shows.map(id =>
+    const API_KEY = process.env.TMDB_API_KEY; // Add to Vercel Environment Variables
+    const showIDs = [25987, 52851]; // your selected TMDb IDs
+    const showPromises = showIDs.map(id =>
         fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}&language=en-US`)
-            .then(r => r.json())
+        .then(r => r.json())
     );
-
     const results = await Promise.all(showPromises);
     res.status(200).json(results);
 }
