7b8d1a3fd2522269447e35e5cb1d5b2a9dede98e
Vystelior <235885728+Vystelior@users.noreply.github.com>
2025-11-18 20:14:19 +0000

    Update shows.js

--- public/js/shows.js
+++ public/js/shows.js
@@ -3,11 +3,12 @@
 // Show mapping
 const showList = [
   // FIX: Ensured TMDB IDs are integers for consistent lookup in series.html
-  { tmdb: 61593, imdb: "tt2950342" }, // Mr Pickles
-  { tmdb: 14769, imdb: "tt1132124" }, // Xavier: Renegade Angel
-  { tmdb: 60625, imdb: "tt2861424" }, // rick and morty
-  { tmdb: 709, imdb: "tt0437745" },   // robot chicken
-  { tmdb: 539, imdb: "tt0457146" },   // squidbillies
-  { tmdb: 251, imdb: "tt0297494" },   // aqua teen hunger force
-  { tmdb: 2723, imdb: "tt0278238" }   // samurai jack
+  { tmdb: 61593, imdb: "tt2950342" },  // Mr Pickles
+  { tmdb: 14769, imdb: "tt1132124" },  // Xavier: Renegade Angel
+  { tmdb: 60625, imdb: "tt2861424" },  // rick and morty
+  { tmdb: 709, imdb: "tt0437745" },    // robot chicken
+  { tmdb: 539, imdb: "tt0457146" },    // squidbillies
+  { tmdb: 251, imdb: "tt0297494" },    // aqua teen hunger force
+  { tmdb: 2723, imdb: "tt0278238" },   // samurai jack
+  { tmdb: 132922, imdb: "tt13641028" } // teenage euthanasia
 ];
