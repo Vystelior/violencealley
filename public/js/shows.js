7507320f9a7b373f471661fbf9c82e08cf87d70c
Vystelior <235885728+Vystelior@users.noreply.github.com>
2025-11-18 20:03:50 +0000

    Update shows.js

--- public/js/shows.js
+++ public/js/shows.js
@@ -4,6 +4,7 @@
 const showList = [
   // FIX: Ensured TMDB IDs are integers for consistent lookup in series.html
   { tmdb: 61593, imdb: "tt2950342" }, // Mr Pickles
-  { tmdb: 14769, imdb: "tt1132124" },  // Xavier: Renegade Angel
-  { tmdb: 60625, imdb: "tt2861424" }  // rick and morty
+  { tmdb: 14769, imdb: "tt1132124" }, // Xavier: Renegade Angel
+  { tmdb: 60625, imdb: "tt2861424" }, // rick and morty
+  { tmdb: 709, imdb: "tt0437745" }    // robot chicken
 ];
